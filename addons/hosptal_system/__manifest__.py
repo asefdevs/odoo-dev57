fd13bfde330652b1667fc810e15d96f1bd22d577
asefdevs <asefdevs@gmail.com>
2025-08-03 13:28:00 +0400

    add sequence logic for appoinment model , for using uniqie and auto implemented referance

--- addons/hosptal_system/__manifest__.py
+++ addons/hosptal_system/__manifest__.py
@@ -5,6 +5,7 @@
     "license": "LGPL-3",
     "data": [
         "security/ir.model.access.csv",
+        "data/sequence.xml",
         "views/patient_views.xml",
         "views/appointment_views.xml",
         "views/menu.xml",
--- addons/hosptal_system/models/appointment.py
+++ addons/hosptal_system/models/appointment.py
@@ -1,11 +1,24 @@
-from odoo import models, fields
+from odoo import models, fields, api
+
+import logging
+_logger = logging.getLogger(__name__)
 
 class Appointment(models.Model):
     _inherit = 'mail.thread'
     _name = 'hospital.appointment'
     _description = 'Hospital Appointment'
-    _rec_name = 'patient_id'
+    _rec_name = 'name'
 
+    name = fields.Char(string='Reference', required=True, copy=False, readonly=True, default="NEW")
     patient_id = fields.Many2one('hospital.patient', string='Patient', required=True, tracking=True)
     appointment_date = fields.Datetime(string='Appointment Date', required=True, tracking=True)
     note = fields.Text(string='Notes', tracking=True)
+
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', 'NEW') == 'NEW':
+            vals['name'] = self.env['ir.sequence'].next_by_code('hospital.appointment') or 'NEW'
+        return super(Appointment, self).create(vals)
+
+
